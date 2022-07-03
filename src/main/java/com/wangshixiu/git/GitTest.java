01e471a165a36e32a76023873743fd812765acc4
wangShiXiu <wangshixiu20@163.com>
2022-07-03 15:41:31 +0800

    Initial commit

---
+++ src/main/java/com/wangshixiu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.wangshixiu.git;
+
+/**
+ * @Author 王世秀
+ * @Date 2022/7/3
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
